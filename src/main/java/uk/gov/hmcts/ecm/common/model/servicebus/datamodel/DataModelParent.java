430cac577cbea9b66e84587d00154149ff90bb56
Javier Carretero <javi_1986@hotmail.com>
2020-06-24 10:13:02 +0100

    Removed AllArgsConstructor

--- src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/datamodel/DataModelParent.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/datamodel/DataModelParent.java
@@ -1,14 +1,12 @@
 package uk.gov.hmcts.ecm.common.model.servicebus.datamodel;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DataModelParent {
--- src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/datamodel/DetachDataModel.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/datamodel/DetachDataModel.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.ecm.common.model.servicebus.datamodel;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,6 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DetachDataModel extends DataModelParent {
