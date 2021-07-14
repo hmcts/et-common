e1f92634271e2d9a79ce3d1e2884455388a33055
gauravchawla <gaurav-chawla@hotmail.com>
2021-07-14 23:51:37 +0100

    new fields added

--- src/main/java/uk/gov/hmcts/ecm/common/model/multiples/MultipleData.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/multiples/MultipleData.java
@@ -68,6 +68,9 @@ public class MultipleData {
 
     @JsonProperty("batchUpdateClaimantRep")
     private DynamicFixedListType batchUpdateClaimantRep;
+    @JsonProperty("batchRemoveClaimantRep")
+    private String batchRemoveClaimantRep;
+
     @JsonProperty("batchUpdateJurisdiction")
     private DynamicFixedListType batchUpdateJurisdiction;
     @JsonProperty("batchUpdateRespondent")
@@ -76,6 +79,8 @@ public class MultipleData {
     private DynamicFixedListType batchUpdateJudgment;
     @JsonProperty("batchUpdateRespondentRep")
     private DynamicFixedListType batchUpdateRespondentRep;
+    @JsonProperty("batchRemoveRespondentRep")
+    private String batchRemoveRespondentRep;
 
     @JsonProperty("managingOffice")
     private String managingOffice;
