8554e2d28c321eda433f32cc1b27048e3db9df9e
hsjhita <harpreet.jhita@justice.gov.uk>
2021-10-20 15:51:44 +0100

    ECM-28

--- src/main/java/uk/gov/hmcts/ecm/common/model/ccd/types/DepositType.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/ccd/types/DepositType.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.ecm.common.model.ccd.types;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import uk.gov.hmcts.ecm.common.model.bulk.types.DynamicFixedListType;
 import uk.gov.hmcts.ecm.common.model.ccd.Document;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -11,8 +12,12 @@ public class DepositType {
 
     @JsonProperty("Deposit_amount")
     private String depositAmount;
+    @JsonProperty("dynamicDepositOrderAgainst")
+    private DynamicFixedListType dynamicDepositOrderAgainst;
     @JsonProperty("depositOrderAgainst")
     private String depositOrderAgainst;
+    @JsonProperty("dynamicDepositRequestedBy")
+    private DynamicFixedListType dynamicDepositRequestedBy;
     @JsonProperty("deposit_requested_by")
     private String depositRequestedBy;
     @JsonProperty("deposit_covers")
@@ -33,6 +38,8 @@ public class DepositType {
     private String depositRefund;
     @JsonProperty("deposit_refund_date")
     private String depositRefundDate;
+    @JsonProperty("dynamicDepositRefundedTo")
+    private DynamicFixedListType dynamicDepositRefundedTo;
     @JsonProperty("depositRefundedTo")
     private String depositRefundedTo;
     @JsonProperty("depositNotes")
