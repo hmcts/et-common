bf158a5f3302812aa9dbda1969faf6fb89de2a46
gauravchawla <gaurav-chawla@hotmail.com>
2022-02-20 16:39:00 +0000

    removed function

--- src/main/java/uk/gov/hmcts/ecm/common/model/bulk/types/DynamicFixedListType.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/bulk/types/DynamicFixedListType.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ecm.common.model.bulk.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Optional;
 import lombok.Data;
 
 import java.util.List;
