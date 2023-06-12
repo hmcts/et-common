6ae9c7fa8f2f4f8323b1a081c362ba248f4e02e4
Itay-Kirsh <Itayk747@gmail.com>
2023-06-12 11:13:59 +0100

    Add updated state

--- src/main/java/uk/gov/hmcts/ecm/common/model/helper/Constants.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/helper/Constants.java
@@ -364,4 +364,5 @@ public class Constants {
     public static final String TRIBUNAL = "Tribunal";
     public static final String NOT_VIEWED_YET = "notViewedYet";
     public static final String NOT_STARTED_YET = "notStartedYet";
+    public static final String UPDATED = "updated";
 }
