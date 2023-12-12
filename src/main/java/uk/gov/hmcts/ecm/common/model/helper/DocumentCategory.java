f8cbaff31526ecd21e7d33c90739714f0e7f068c
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-12-12 09:20:22 +0000

    CFV changes

--- src/main/java/uk/gov/hmcts/ecm/common/model/helper/DocumentCategory.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/helper/DocumentCategory.java
@@ -17,7 +17,6 @@ public enum DocumentCategory {
     JUDGMENT_AND_REASONS("C7", "Judgment and Reasons"),
     RECONSIDERATION("C8", "Reconsideration"),
     MISC("C9", "Misc"),
-    LEGACY_DOCUMENT_NAMES("C10", "Legacy Document Names"),
     ET1("C11", "ET1", "C1"),
     ET1_ATTACHMENT("C12", "ET1 Attachment", "C1"),
     ACAS_CERTIFICATE("C13", "ACAS Certificate", "C1"),
@@ -41,6 +40,8 @@ public enum DocumentCategory {
     APP_TO_VARY_AN_ORDER_R("C31", "App to vary an order – R", "C4"),
     APP_TO_REVOKE_AN_ORDER_C("C32", "App to revoke an order- C", "C4"),
     APP_TO_REVOKE_AN_ORDER_R("C33", "App to revoke an order – R", "C4"),
+    APP_TO_VARY_OR_REVOKE_AN_ORDER_C("C73", "App to vary or revoke an order - C", "C4"),
+    APP_TO_VARY_OR_REVOKE_AN_ORDER_R("C74", "App to vary or revoke an order - R", "C4"),
     APP_TO_EXTEND_TIME_TO_COMPLY_TO_AN_ORDER_DIRECTIONS_C("C34",
             "App to extend time to comply to an order/directions – C", "C4"),
     APP_TO_EXTEND_TIME_TO_COMPLY_TO_AN_ORDER_DIRECTIONS_R("C35",
@@ -50,7 +51,7 @@ public enum DocumentCategory {
     APP_TO_AMEND_CLAIM("C38", "App to amend claim", "C4"),
     APP_TO_AMEND_RESPONSE("C39", "App to amend response", "C4"),
     APP_FOR_A_WITNESS_ORDER_C("C40", "App for a Witness Order - C", "C4"),
-    APP_FOR_A_WITNESS_ORDER_R("C40", "App for a Witness Order - R", "C4"),
+    APP_FOR_A_WITNESS_ORDER_R("C75", "App for a Witness Order - R", "C4"),
     DISABILITY_IMPACT_STATEMENT("C41", "Disability Impact statement", "C4"),
     R_HAS_NOT_COMPLIED_WITH_AN_ORDER_C("C42", "R has not complied with an order - C", "C4"),
     C_HAS_NOT_COMPLIED_WITH_AN_ORDER_R("C43", "C has not complied with an order - R", "C4"),
@@ -60,7 +61,7 @@ public enum DocumentCategory {
     CHANGE_OF_PARTY_DETAILS("C47", "Change of party's details", "C4"),
     WITHDRAWAL_OF_ENTIRE_CLAIM("C48", "Withdrawal of entire claim", "C5"),
     WITHDRAWAL_OF_PART_OF_CLAIM("C49", "Withdrawal of part of claim", "C5"),
-    WITHDRAWAL_OF_ALL_OR_PART_CLAIM("C49", "Withdrawal of all or part of claim", "C5"),
+    WITHDRAWAL_OF_ALL_OR_PART_CLAIM("C78", "Withdrawal of all or part of claim", "C5"),
     COT3("C50", "COT3", "C5"),
     APP_TO_RESTRICT_PUBLICITY_C("C51", "App to restrict publicity - C", "C6"),
     APP_TO_RESTRICT_PUBLICITY_R("C52", "App to restrict publicity - R", "C6"),
@@ -85,7 +86,9 @@ public enum DocumentCategory {
     TRIBUNAL_CASE_FILE("C69", "Tribunal Case File", "C9"),
     OTHER("C70", "Other", "C9"),
     ET1_VETTING("C71", "ET1 Vetting", "C1"),
-    ET3_PROCESSING("C72", "ET3 Processing", "C2");
+    ET3_PROCESSING("C72", "ET3 Processing", "C2"),
+    CONTACT_THE_TRIBUNAL_C("C76", "Contact the tribunal about something else - C", "C4"),
+    CONTACT_THE_TRIBUNAL_R("C77", "Contact the tribunal about something else - R", "C4");
 
     private final String id;
     private final String category;
