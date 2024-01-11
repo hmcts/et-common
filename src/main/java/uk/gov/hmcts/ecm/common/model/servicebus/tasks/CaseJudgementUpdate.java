b8f32ffc0bc7db38dd22f5107f27492137965414
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-01-11 12:08:15 +0000

    Update CaseJudgementUpdate.java

--- src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/tasks/CaseJudgementUpdate.java
+++ src/main/java/uk/gov/hmcts/ecm/common/model/servicebus/tasks/CaseJudgementUpdate.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.et.common.model.ccd.types.JudgementType;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.NO;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
