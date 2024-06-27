245a7c456f652c3d5b68e86642081825166f90e0
JackReevies <43950645+JackReevies@users.noreply.github.com>
2024-06-27 15:17:04 +0100

    RET-5232: Added jurisdictions list (#554)

--- src/test/java/uk/gov/hmcts/ecm/common/model/servicebus/tasks/UpdateDataModelBuilder.java
+++ src/test/java/uk/gov/hmcts/ecm/common/model/servicebus/tasks/UpdateDataModelBuilder.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.ecm.common.model.servicebus.tasks;
 import uk.gov.hmcts.ecm.common.model.servicebus.datamodel.UpdateDataModel;
 import uk.gov.hmcts.et.common.model.ccd.types.JurCodesType;
 
+import java.util.ArrayList;
+
 public class UpdateDataModelBuilder {
     private final UpdateDataModel dataModel = new UpdateDataModel();
 
@@ -11,7 +13,9 @@ public class UpdateDataModelBuilder {
         jurCodeType.setJuridictionCodesList(jurCode);
         jurCodeType.setJudgmentOutcome(outcome);
 
-        dataModel.setJurCodesType(jurCodeType);
+        ArrayList<JurCodesType> list = new ArrayList<>();
+        list.add(jurCodeType);
+        dataModel.setJurCodesList(list);
         return this;
     }
 
